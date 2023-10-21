e338719a3fdca7d83ee712a29004bc3d37b7d0af
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-10-21 22:58:53 +0900

    feat:nextra template

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # ExtremeRoles.Doc
-- ExtremeRoles.wikiをjust-the-docsによって作り替えたWiki
-
-## Special Thanks
-- ExtremeRoles.Docはこのテーマを使っています：https://github.com/just-the-docs/just-the-docs
-- ExtremeRoles.Docはこのテンプレートを使用して作成しています：https://github.com/just-the-docs/just-the-docs-template
+- nextraへ移行中・・・
---
+++ theme.config.js
@@ -0,0 +1,21 @@
+export default {
+    useNextSeoProps() {
+        return {
+            titleTemplate: '%s - ExtremeRoles'
+        }
+    },
+    project: {
+        link: 'https://github.com/yukieiji/ExtremeRoles',
+    },
+    chat: {
+        link: 'https://discord.com/invite/UzJcfBYcyS',
+    },
+    docsRepositoryBase: 'https://github.com/yukieiji/ExtremeRoles',
+    footer: {
+        text: 'ExtremeRoles は、CC by 4.0 の下に提供されています',
+    },
+    i18n: [
+        { locale: 'ja-JP', text: '日本語' },
+        { locale: 'en-US', text: 'English' },
+    ]
+}
