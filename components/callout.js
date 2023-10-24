b889b9499c9ddb9bfbaafcf6b03c807c14d87a21
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-10-24 21:57:41 +0900

    refactor:Callouts

--- components/callout.js
+++ components/callout.js
@@ -2,35 +2,24 @@ import s from "/css/callout.module.css";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faCircleExclamation, faNoteSticky, faTriangleExclamation} from "@fortawesome/free-solid-svg-icons";
 import {useRouter} from "next/router";
+
 export default function CustomCallout({ children, type, titleSize=16 }) {
-    const { locale } = useRouter()
-    let cn;
-    let warning;
-    let important;
-    let note;
-    if(locale === "ja-JP") {
-        warning = "注意"
-        important = "重要"
-        note = "ノート";
-    } else if(locale === "en-US") {
-        warning = "Warning"
-        important = "Important"
-        note = "Note";
-    }
-    if (type === "warning") {
-        cn = `${s.warning} ${s.box}`;
-    } else if (type === "important") {
-        cn = `${s.important} ${s.box}`;
-    } else if (type === "note") {
-        cn = `${s.note} ${s.box}`;
-    }
+
+    const className = GetCalloutClassName(type)
+    const titleClassName = GetCalloutTitleClassName(type)
+    const icon = GetIcon(type)
+    const title = GetTitele(type)
+
     return (
         <>
-            <div className={cn}>
+            <div className={className}>
                 <div>
-                    {type === "warning" && <div className={`${s.title} ${s.warningtitle}`} style={{fontSize: titleSize}}> <FontAwesomeIcon icon={faTriangleExclamation} width={titleSize}/><p>{warning}</p> </div>}
-                    {type === "important" && <div className={`${s.title} ${s.importanttitle}`} style={{fontSize: titleSize}}> <FontAwesomeIcon icon={faCircleExclamation} width={titleSize}/> <p>{important}</p> </div>}
-                    {type === "note" && <div className={`${s.title} ${s.notetitle}`} style={{fontSize: titleSize}}> <FontAwesomeIcon icon={faNoteSticky} width={titleSize}/> <p>{note}</p> </div>}
+                    <div className={titleClassName} style={{fontSize: titleSize}}>
+                        <FontAwesomeIcon icon={icon} width={titleSize}/>
+                        <p>
+                            {title}
+                        </p>
+                    </div>
                 </div>
                 <div className={s.texts}>
                     <div>{children}</div>
@@ -38,4 +27,127 @@ export default function CustomCallout({ children, type, titleSize=16 }) {
             </div>
         </>
     );
+}
+
+function GetCalloutClassName(type)
+{
+    let cn;
+
+    switch(type)
+    {
+        case "warning":
+            cn = `${s.warning}`;
+            break
+        case "important":
+            cn = `${s.important}`;
+            break
+        case "note":
+            cn = `${s.note}`;
+            break
+        default:
+            break
+    }
+
+    return cn + ` ${s.box}`;
+}
+
+function GetCalloutTitleClassName(type)
+{
+    let cn = `${s.title} `;
+
+    switch(type)
+    {
+        case "warning":
+            cn += `${s.warningtitle}`;
+            break
+        case "important":
+            cn += `${s.importanttitle}`;
+            break
+        case "note":
+            cn += `${s.notetitle}`;
+            break
+        default:
+            break
+    }
+
+    return cn;
+}
+
+function GetTitele(type)
+{
+    switch(type)
+    {
+        case "warning":
+            return GetWarningTransData();
+        case "important":
+            return GetImportantTransData();
+        case "note":
+            return GetNoteTransData();
+        default:
+            return ``;
+    }
+}
+
+function GetIcon(type)
+{
+    switch(type)
+    {
+        case "warning":
+            return faTriangleExclamation;
+        case "important":
+            return faCircleExclamation;
+        case "note":
+            return faNoteSticky;
+        default:
+            return ``;
+    }
+}
+
+
+function GetWarningTransData()
+{
+
+    const { locale, _ } = useRouter()
+
+    switch(locale)
+    {
+        case "ja-JP":
+            return "注意"
+        case "en-US":
+            return "Warning"
+        default:
+            return ``;
+    }
+}
+
+function GetImportantTransData()
+{
+
+    const { locale, _ } = useRouter()
+
+    switch(locale)
+    {
+        case "ja-JP":
+            return "重要"
+        case "en-US":
+            return "Important"
+        default:
+            return ``;
+    }
+}
+
+function GetNoteTransData()
+{
+
+    const { locale, _ } = useRouter()
+
+    switch(locale)
+    {
+        case "ja-JP":
+            return "ノート"
+        case "en-US":
+            return "Note"
+        default:
+            return ``;
+    }
 }
